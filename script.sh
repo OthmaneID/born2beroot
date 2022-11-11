0af3ebf11dbcd42dcf484430d5cfe98d3e857663
OthmaneID <74469949+OthmaneID@users.noreply.github.com>
2022-11-11 11:36:13 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -14,3 +14,5 @@ grep 'cpu ' /proc/stat | awk '{printf "CPU load : %.1f%%\n", ($2+$4)*100/($2+$4+
 who -b | awk '{print "#Last boot: "$3" "$4}'
 #TCP connection
 ss -s | grep "estab" | tr -d ',' | awk '{print "#Connections TCP : "$4}'
+# to get the connected users (I THINK USE)
+who / id -un / w -u
